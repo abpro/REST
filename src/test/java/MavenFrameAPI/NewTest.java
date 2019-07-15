5b36ec7f1cb81de127a6ea847268fecd3f129716
ankur <ankurbakshi03@gmail.com>
2019-07-15 16:03:05 +0530

    REST BEGINS

---
+++ src/test/java/MavenFrameAPI/NewTest.java
@@ -0,0 +1,10 @@
+package MavenFrameAPI;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("Hello World! Hello from the Maven Side");
+  }
+}
